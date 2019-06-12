9673fb70f865529cc1e0d39589c86cbc710502b3
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-12 11:13:05 -0400

    Registro Analisis Medicos

--- AnalisisMedicos/Form1.cs
+++ AnalisisMedicos/Form1.cs
@@ -16,5 +16,10 @@ namespace AnalisisMedicos
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
