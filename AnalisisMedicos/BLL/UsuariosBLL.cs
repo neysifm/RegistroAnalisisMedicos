c9cd969e564afb5ebc3cd65419d452afabb4962d
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-13 09:23:40 -0400

    BLL

---
+++ AnalisisMedicos/BLL/UsuariosBLL.cs
@@ -0,0 +1,115 @@
+﻿using AnalisisMedicos.DAL;
+using AnalisisMedicos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisMedicos.BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool Guardar(Usuarios usuario)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.Usuarios.Add(usuario) != null)
+                    paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int Id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var eliminar = contexto.Usuarios.Find(Id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Usuarios usuario)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(usuario).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Usuarios Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Usuarios usuario = new Usuarios();
+            try
+            {
+                usuario = contexto.Usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return usuario;
+        }
+
+        public static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> usuario)
+        {
+            List<Usuarios> Lista = new List<Usuarios>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                Lista = contexto.Usuarios.Where(usuario).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
